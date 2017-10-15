e4e20c53a9d06627bada61b9292c60a69a4c2e73
StevenLW8 <31933789+StevenLW8@users.noreply.github.com>
2017-10-15 17:53:48 -0400

    Change1

--- src/TimesTablePrint.java
+++ src/TimesTablePrint.java
@@ -9,23 +9,36 @@ public class TimesTablePrint {
 		int high;
 		int low;
 		int num=0;
+		boolean check=false;
+	
 		//Ask the user for input
 		System.out.println("What is the biggest number that you want?");
 		high=in.nextInt();
 		System.out.println("What is the smallest number that you want?");
 		low=in.nextInt();
 		//Print the times table
+		if (check ==true) {
+			
+		
 		for(int low1=low;low1<high;low1++) {
 			System.out.println("\n"+low1);
+		}
 
 		}
 		while (low<=high) {
-
-
+			
 			System.out.print("\t"+low);
+			
+			
 
 			low+=1;
+		
 			}
+		for(int x=1;x<low;x++) { 
+			System.out.println("\n");
+			System.out.print("\t"+x+"\t"+x*x);
+			
+		}
 			
 
 		}
